34649c05412f1fdc41bb70968272ca9b05b2e9bc
suannelson123 <nelsonsuan12@gmail.com>
2023-09-28 12:24:08 +0800

    hmtl-css-java

--- hmtl-css-java/index.js
+++ hmtl-css-java/index.js
@@ -1,1 +1,15 @@
-console .log ( "Hello World!" );
+console.log("Hello World!");
+
+let FirstName = "Nelson";
+let student = true;
+let age = 19;
+age = age + 1;
+
+console.log("Hello", FirstName);
+console.log("Enrolled:", student);
+console.log("You are", age, "yers old");
+
+
+document.getElementById("header1").innerHTML = "Hello " + FirstName;
+document.getElementById("header2").innerHTML = "You are " + age + " years old ";
+document.getElementById("header3").innerHTML = "Enrolled: " + student;
